b3b0933366f6f1fed584309ad0780b700e6c08f5
Syaiful Dwi Anwar <vlack123@gmail.com>
2025-01-14 12:22:42 +0700

    cso

--- app/Http/Controllers/KabupatenController.php
+++ app/Http/Controllers/KabupatenController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 
 class KabupatenController extends Controller
 {
+  
   /**
    * Display a listing of the resource.
    */
--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,14 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::middleware(['auth:sanctum'])->group(function() {
-  Route::apiResource('propinsis', PropinsiController::class)->except(['index', 'show']);
+  Route::apiResource('propinsis', PropinsiController::class);
   Route::apiResource('kabupatens', KabupatenController::class)->except(['index', 'show']);
   Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
-Route::get('propinsis', [PropinsiController::class, 'index'])->name('kabupaten.index');
-Route::get('propinsis', [PropinsiController::class, 'show'])->name('kabupaten.show');
+// Route::get('propinsis', [PropinsiController::class, 'index'])->name('propinsi.index');
+// Route::get('propinsis', [PropinsiController::class, 'show'])->name('propinsi.show');
 Route::get('kabupatens', [KabupatenController::class, 'index'])->name('kabupaten.index');
-Route::get('kabupatens', [KabupatenController::class, 'show'])->name('kabupaten.show');
+Route::get('kabupatens/{kabupaten}', [KabupatenController::class, 'show'])->name('kabupaten.show');
 
 // Route::apiResource('propinsis', PropinsiController::class);
 // Route::post('/register', [AuthController::class, 'register']);
